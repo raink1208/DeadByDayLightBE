0b69f6d359a0043d8aba3d5d4bd16b6db89ae1ba
raink1208 <58714901+raink1208@users.noreply.github.com>
2020-07-24 02:22:37 +0900

    add onSneak

--- DeadByDayLightBE/src/rain1208/deadByDayLight/listener/PlayerEventListener.php
+++ DeadByDayLightBE/src/rain1208/deadByDayLight/listener/PlayerEventListener.php
@@ -5,8 +5,15 @@ namespace rain1208\deadByDayLight\listener;
 
 
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerToggleSneakEvent;
+use rain1208\deadByDayLight\Main;
 
 class PlayerEventListener implements Listener
 {
-
+    public function onSneak(PlayerToggleSneakEvent $event)
+    {
+        if (Main::getInstance()->gameExists()) {
+            Main::getInstance()->getGame()->setFlag($event->getPlayer(),$event->isSneaking());
+        }
+    }
 }
